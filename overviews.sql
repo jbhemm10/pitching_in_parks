514bf2a0d9cd7833d718ac06f338be6ff26f08fd
jbhemm10 <jhemmelg@purdue.edu>
2025-09-26 16:02:46 -0500

    Updated query to include at bat outcome

--- overviews.sql
+++ overviews.sql
@@ -17,17 +17,40 @@ ORDER BY total_pitches DESC;
 
 -- Collects all pitch types at each stadium and counts them, with strike/ball/in play breakdown
 SELECT 
-    home_team as stadium,
-    pitch_type,
+    p.home_team as stadium,
+    p.pitch_type,
+    pa.pitch_name,
     COUNT(*) AS total_pitches,
-    COUNT(CASE WHEN description = 'called_strike' OR description = 'swinging_strike' or description = 'foul' OR description = 'foul_tip' or description = 'foul_bunt' or description = 'missed_bunt' THEN 1 END) AS strike_count,
-    COUNT(CASE WHEN description = 'ball' or description = 'blocked_ball' or description = 'hit_by_pitch' THEN 1 END) AS ball_count,
-    COUNT(CASE WHEN description = 'hit_into_play' THEN 1 END) AS in_play_count,
-    COUNT(CASE WHEN events = 'single' THEN 1 END) AS single_count,
-    COUNT(CASE WHEN events = 'double' THEN 1 END) AS double_count,
-    COUNT(CASE WHEN events = 'triple' THEN 1 END) AS triple_count,
-    COUNT(CASE WHEN events = 'home_run' THEN 1 END) AS home_run_count
-FROM dbo.[2024_pitch_by_pitch_data]
-GROUP BY home_team, pitch_type
-ORDER BY home_team, total_pitches DESC;
+    COUNT(CASE WHEN p.description = 'called_strike' OR p.description = 'swinging_strike' or p.description = 'foul' OR p.description = 'foul_tip' or p.description = 'foul_bunt' or p.description = 'missed_bunt' THEN 1 END) AS strike_count,
+    COUNT(CASE WHEN p.description = 'ball' or p.description = 'blocked_ball' or p.description = 'hit_by_pitch' THEN 1 END) AS ball_count,
+    COUNT(CASE WHEN p.description = 'hit_into_play' THEN 1 END) AS in_play_count,
+    COUNT(CASE WHEN p.events = 'single' THEN 1 END) AS single_count,
+    COUNT(CASE WHEN p.events = 'double' THEN 1 END) AS double_count,
+    COUNT(CASE WHEN p.events = 'triple' THEN 1 END) AS triple_count,
+    COUNT(CASE WHEN p.events = 'home_run' THEN 1 END) AS home_run_count,
+    COUNT(CASE WHEN p.events = 'sac_fly' THEN 1 END) AS sac_fly_count,
+    COUNT(CASE WHEN p.events = 'sac_bunt' THEN 1 END) AS sac_bunt_count,
+    COUNT(CASE WHEN p.events = 'field_out' THEN 1 END) AS field_out_count,
+    COUNT(CASE WHEN p.events = 'force_out' THEN 1 END) AS force_out_count,
+    COUNT(CASE WHEN p.events = 'fielders_choice' THEN 1 END) AS fielders_choice_count,
+    COUNT(CASE WHEN p.events = 'fielders_choice_out' THEN 1 END) AS fielders_choice_out_count,
+    COUNT(CASE WHEN p.events = 'grounded_into_double_play' THEN 1 END) AS gidp_count,
+    COUNT(CASE WHEN p.events = 'double_play' THEN 1 END) AS double_play_count,
+    COUNT(CASE WHEN p.events = 'field_error' THEN 1 END) AS error_count  
+FROM dbo.[2024_pitch_by_pitch_data] p
+INNER JOIN dbo.pitch_abbreviations pa
+    ON p.pitch_type = pa.pitch_abbreviation
+GROUP BY p.home_team, p.pitch_type, pa.pitch_name
+ORDER BY p.home_team, total_pitches DESC;
 
+-- Join pitch data with full name for pitches
+SELECT 
+p.home_team as stadium,
+p.pitch_type,
+pa.pitch_name,
+COUNT(*) AS total_pitches
+FROM dbo.[2024_pitch_by_pitch_data] p
+INNER JOIN dbo.pitch_abbreviations pa
+    ON p.pitch_type = pa.pitch_abbreviation
+GROUP BY p.home_team, p.pitch_type, pa.pitch_name
+ORDER BY p.home_team, total_pitches DESC;
